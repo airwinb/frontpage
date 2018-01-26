c316d8309f13475454e02e304a3e69a02c8ff755
airwinb <erwin@heron3.nl>
2018-01-26 20:50:16 +0100

    Update of Algemeen layout

--- frontpage/js/frontpage_settings_user.js
+++ frontpage/js/frontpage_settings_user.js
@@ -10,27 +10,33 @@ $(document).ready(function () {
 //        ['0', 'Desc',               'cell_p1_1',    'Woonkamer', '0', '0'], //Desc means show the sub cell_p1_s
 //        ['98', 'Temp',              'cell_p1_1a',   'Woonkamer', '1', '0'], //Lastseen only from cell_p1__a possible
 //        ['154', 'Humidity',         'cell_p1_1b',   'Woonkamer', '0', '0'],
-        ['98', 'Temp',              'cell_p1_1',    'Woonkamer', '1', '0'], //Lastseen only from cell_p1__a possible
+        ['0', 'Desc',               'cell_p1_1',    'Binnen / buiten', '0', '0'],
+        ['98', 'Temp',              'cell_p1_1a',   'Binnen / buiten', '1', '0'],
+        ['99', 'Temp',              'cell_p1_1b',   'Binnen / buiten', '0', '0'],
         ['0', 'Desc',               'cell_p1_2',    'Badkamer', '0', '0'],
         ['125', 'Temp',             'cell_p1_2a',   'Badkamer', '1', '0'],
         ['125', 'Humidity',         'cell_p1_2b',   'Badkamer', '0', '0'],
-        ['99', 'Temp',              'cell_p1_3',    'Buiten', '1', '0'],
-        ['0', 'SunBoth',            'cell_p1_4',    'SunBoth', '1', '4'],
+        ['0', 'Desc',               'cell_p1_3',    'Elektra', '0', '0'],
+        ['72', 'Usage',             'cell_p1_3a',   'Elektra', '1', '4'],
+        ['72', 'CounterToday',      'cell_p1_3b',   'Elektra', '0', '4'],
+        ['0', 'Desc',               'cell_p1_4',    'min / max', '0', '0'],
+        ['114', 'Data',             'cell_p1_4a',   'min / max', '1', '4'],
+        ['115', 'Data',             'cell_p1_4b',   'min / max', '0', '4'],
 
         ['97', 'SetPoint',          'cell_p1_5',    'Thermostaat', '0', '1'],
-        ['344', 'Level',            'cell_p1_6',    'Aanrecht licht', '1', '5'],
+        ['0', 'SunBoth',            'cell_p1_6',    'SunBoth', '1', '4'],
         ['17', 'Status',            'cell_p1_7',    'Ventilatie', '0', '1'],
-        ['0', 'Level',              'cell_p1_8',    '', '0', '0'], // see scenes below
+        ['33', 'Status',            'cell_p1_8',    'Power Cube', '0', '1'],
         
-        ['260', 'Status',           'cell_p1_9',    'Garagedeur', '2', '8'],
-        ['73', 'CounterToday',      'cell_p1_10',   'Gas vandaag', '1', '4'],
-        ['33', 'Status',            'cell_p1_11',   'Power Cube', '0', '1'],
-        ['8', 'Status',             'cell_p1_12',   'Kerstboom', '0', '1'],
+        ['565', 'Status',           'cell_p1_9',    'Kast Dim', '0', '1'],
+        ['564', 'Status',           'cell_p1_10',   'Kast Fel', '0', '1'],
+        ['', '',                    'cell_p1_11',   '-', '0', '1'],
+        ['353', 'Status',           'cell_p1_12',   'Oppas', '0', '1'],
         
-        ['72', 'Usage',             'cell_p1_13',   'Elektra nu', '1', '4'],
-        ['72', 'CounterToday',      'cell_p1_14',   'Elektra vandaag', '1', '4'],
-        ['114', 'Data',             'cell_p1_15',   'Elektra min', '1', '4'],
-        ['115', 'Data',             'cell_p1_16',   'Elektra max', '1', '4'],
+        ['484', 'Status',           'cell_p1_13',   'Garagedeur', '2', '8'],
+        ['73', 'CounterToday',      'cell_p1_14',   'Gas vandaag', '1', '4'],
+        ['503', 'Data',             'cell_p1_15',   'Erwin', '1', '4'],
+        ['504', 'Data',             'cell_p1_16',   'Liesbeth', '1', '4'],
     
         // page 2
         ['269', 'Status',           'cell_p2_1',    'Overloop', '0', '1'],
@@ -55,7 +61,7 @@ $(document).ready(function () {
 
     ];
     $.PageArray_Scenes = [
-        ['1', 'Status',        'cell_p1_8',    'Lampen kamer', '0', '1']
+        ['1', 'Status',        'cell_p1_11',    'Lampen kamer', '0', '1']
         // ['7', 'Status',        'cell_p1_13',   'Lamp achtertuin', '1', '0'],
     ];
 
